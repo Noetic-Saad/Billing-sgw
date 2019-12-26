3ed94ac3006825174b2a6dcb03d67397d3f07419
Furqan Ghauri <furqan@noeticworld.com>
2019-12-26 16:41:20 +0500

    missing file

--- src/main/java/com/noetic/sgw/billing/sgwbilling/util/Response.java
+++ src/main/java/com/noetic/sgw/billing/sgwbilling/util/Response.java
@@ -4,6 +4,7 @@ public class Response {
 
     private int code;
     private String msg;
+    private String correlationId;
 
     public int getCode() {
         return code;
@@ -19,5 +20,13 @@ public class Response {
 
     public void setMsg(String msg) {
         this.msg = msg;
+    }
+
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
     }
 }
